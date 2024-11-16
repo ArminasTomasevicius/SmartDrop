daf4b9d2b6a8b7a1e36bc8aa8f180d90e182cd36
erikasnar <naerikas@gmail.com>
2024-11-16 23:26:58 +0200

    Adding initial leaderboard layout

--- src/app/leaderbord/page.tsx
+++ src/app/leaderbord/page.tsx
@@ -1,5 +1,87 @@
-import { Box } from "@chakra-ui/react";
+import { chakra, Flex, Input, Text } from "@chakra-ui/react";
 
 export default function Leaderbord() {
-  return <Box>Leaderbord</Box>;
+  const leaderbord = [
+    {
+      name: "amacaseybae",
+      points: "0.00102",
+    },
+    {
+      name: "amacaseybae",
+      points: "0.00102",
+    },
+    {
+      name: "amacaseybae",
+      points: "0.00102",
+    },
+    {
+      name: "amacaseybae",
+      points: "0.00102",
+    },
+    {
+      name: "amacaseybae",
+      points: "0.00102",
+    },
+    {
+      name: "amacaseybae",
+      points: "0.00102",
+    },
+    {
+      name: "amacaseybae",
+      points: "0.00102",
+    },
+    {
+      name: "amacaseybae",
+      points: "0.00102",
+    },
+    {
+      name: "amacaseybae",
+      points: "0.00102",
+    },
+    {
+      name: "amacaseybae",
+      points: "0.00102",
+    },
+  ];
+  return (
+    <Flex direction="column" align="center" mt={16}>
+      <Text fontSize="4xl" fontWeight="bold">
+        LEADERBORD
+      </Text>
+      <Text>You are rank 0 in total of 31,694 unique farmers.</Text>
+      <Input
+        placeholder="Search.."
+        type="text"
+        w="100%"
+        maxW="300px"
+        my={8}
+        borderRadius="2xl"
+        borderColor="#454545"
+        _placeholder={{
+          color: "#A2A2A2",
+        }}
+        _hover={{
+          borderColor: "#4ae292",
+        }}
+        _focus={{
+          boxShadow: "none",
+          borderColor: "#4ae292",
+        }}
+      />
+
+      <Flex direction="column">
+        {leaderbord.map((user, index) => (
+          <Flex
+            borderTop="1px solid #454545"
+            borderBottom="1px solid #454545"
+            p={4}
+          >
+            <Text mr={4}>{index + 1}</Text>
+            <Text mr={64}>{user.name}</Text>
+            <Text>{user.points}</Text>
+          </Flex>
+        ))}
+      </Flex>
+    </Flex>
+  );
 }
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,17 +3,16 @@ import { Flex, Text } from "@chakra-ui/react";
 export default function Footer({}: {}) {
   return (
     <Flex
+      as="footer"
       w="95%"
       h={5}
       align="center"
       justify="space-between"
       borderTop="1px solid #454545"
       borderBottom="1px solid #454545"
-      position="fixed"
-      bottom={5}
       py={5}
-      left="50%"
-      transform="translateX(-50%)"
+      mx="auto"
+      my={8}
     >
       <Text px={8}>SMARTDROP</Text>
       <Text px={8}>2024</Text>
