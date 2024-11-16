4b8bf5a343da774c5d5968be913855fffcd3ee87
erikasnar <naerikas@gmail.com>
2024-11-16 23:47:22 +0200

    Updating chains

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,14 +1,27 @@
 import "@rainbow-me/rainbowkit/styles.css";
-import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { WagmiProvider } from "wagmi";
-import { mainnet, polygon, optimism, arbitrum, base } from "wagmi/chains";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import {
+  mainnet,
+  lineaTestnet,
+  zircuitTestnet,
+  unichainSepolia,
+  scrollSepolia,
+} from "wagmi/chains";
 
+if (!process.env.NEXT_PUBLIC_PROJECT_ID) {
+  throw new Error("Project ID not defined");
+}
 export const config = getDefaultConfig({
-  appName: "My RainbowKit App",
-  projectId: "YOUR_PROJECT_ID",
-  chains: [mainnet, polygon, optimism, arbitrum, base],
-  ssr: true, // If your dApp uses server side rendering (SSR)
+  appName: "SmartDrop",
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  chains: [
+    mainnet,
+    lineaTestnet,
+    zircuitTestnet,
+    unichainSepolia,
+    scrollSepolia,
+  ],
+  ssr: true,
 });
 
 export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
