98c4835831d6c019a460216e90ff207c1a6b69d2
erikasnar <naerikas@gmail.com>
2024-11-17 03:23:10 +0200

    Extending wagmi config

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -5,24 +5,13 @@ import {
   zircuitTestnet,
   unichainSepolia,
   scrollSepolia,
+  sepolia,
 } from "@wagmi/core/chains";
 import { http, createConfig } from "@wagmi/core";
 
 if (!process.env.NEXT_PUBLIC_PROJECT_ID) {
   throw new Error("Project ID not defined");
 }
-// export const config = getDefaultConfig({
-//   appName: "SmartDrop",
-//   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-//   chains: [
-//     mainnet,
-//     lineaTestnet,
-//     zircuitTestnet,
-//     unichainSepolia,
-//     scrollSepolia,
-//   ],
-//   ssr: true,
-// });
 
 export const wagmiCoreConfig = createConfig({
   chains: [
@@ -38,6 +27,7 @@ export const wagmiCoreConfig = createConfig({
     [zircuitTestnet.id]: http(),
     [unichainSepolia.id]: http(),
     [scrollSepolia.id]: http(),
+    [sepolia.id]: http("https://eth-sepolia.blockscout.com/api/eth-rpc"),
   },
 });
 
